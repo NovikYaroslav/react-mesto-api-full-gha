cc3464286f4ae1134eac7d2ef4497c5853bfc349
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-22 00:15:22 +0200

    final

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const router = require('./routers');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 //   'http://mesto.novik.nomoredomains.work',
 // ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 200,
@@ -33,35 +33,31 @@ app.listen(PORT);
 app.use(express.json());
 app.use(helmet());
 app.use(limiter);
-// app.use(
-//   cors({
-//     origin: [
-//       'https://mesto.novik.nomoredomains.work',
-//       'http://mesto.novik.nomoredomains.work',
-//     ],
-//   }),
-// );
+app.use(
+  cors({
+    origin: [
+      'https://mesto.novik.nomoredomains.work',
+      'http://mesto.novik.nomoredomains.work',
+    ],
+  }),
+);
 app.use(requestLogger);
 // eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  const { method } = req;
-  // const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://mesto.novik.nomoredomains.work',
-  );
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   const { method } = req;
+//   const { origin } = req.headers;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   res.header('Access-Control-Allow-Credentials', true);
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   next();
+// });
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
