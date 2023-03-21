b65cd863af1aa23813e34b4b11174cdb08f888b4
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-21 23:56:32 +0200

    test cors

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,10 @@ const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
-const allowedCors = [
-  'https://mesto.novik.nomoredomains.work',
-  'http://mesto.novik.nomoredomains.work',
-];
+// const allowedCors = [
+//   'https://mesto.novik.nomoredomains.work',
+//   'http://mesto.novik.nomoredomains.work',
+// ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const limiter = rateLimit({
@@ -45,12 +45,13 @@ app.use(requestLogger);
 // eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
   const { method } = req;
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
   res.header('Access-Control-Allow-Credentials', true);
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
