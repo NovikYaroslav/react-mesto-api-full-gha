382af8ab5a2a8a824d464a99d0973d5f799c920d
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-22 00:06:06 +0200

    next cors fix

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,10 @@ app.use((req, res, next) => {
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', origin);
   // }
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://mesto.novik.nomoredomains.work',
+  );
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
