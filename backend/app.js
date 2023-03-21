5f8c8ecec4810d11c8154f91f24b0946af95cfe0
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-03-21 16:48:05 +0200

    new urls and cors added

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.listen(PORT);
 app.use(express.json());
 app.use(helmet());
 app.use(limiter);
-app.use(cors({ origin: 'http://localhost:3001' }));
+app.use(cors({ origin: 'http://mesto.novik.nomoredomains.work' }));
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
